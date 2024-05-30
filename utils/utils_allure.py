d8406fbfc73e597bddc3f9223ade2e9262594912
Daria Jakuszewicz <d.jakuszewicz@gmail.com>
2024-05-30 15:16:33 +0300

    каталог для аттачей

---
+++ utils/utils_allure.py
@@ -0,0 +1,30 @@
+import json
+import os
+import allure
+
+
+def attach_bstack_video(session_id):
+
+    import requests
+    bstack_session = requests.get(
+        f'https://api.browserstack.com/app-automate/sessions/{session_id}.json',
+        auth=(os.getenv('BS_USERNAME'), os.getenv('BS_ACCESSKEY')),
+    ).json()
+
+    allure.attach(
+        json.dumps(bstack_session),
+        name='Session data',
+        attachment_type=allure.attachment_type.JSON,
+    )
+
+    video_url = bstack_session['automation_session']['video_url']
+
+    allure.attach(
+        '<html><body>'
+        '<video width="100%" height="100%" controls autoplay>'
+        f'<source src="{video_url}" type="video/mp4">'
+        '</video>'
+        '</body></html>',
+        name='video recording',
+        attachment_type=allure.attachment_type.HTML,
+    )
