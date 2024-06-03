335bf26d5ed07dfab573acc7960b38697c116ec7
Daria Jakuszewicz <d.jakuszewicz@gmail.com>
2024-06-03 20:13:37 +0300

    Улучшила структуру конфига

--- config.py
+++ config.py
@@ -7,10 +7,6 @@ context = os.getenv('CONTEXT', 'local_emulator')
 
 load_dotenv(file.abs_path_from_project(f'.env.{context}'))
 
-runs_on_bstack = (context == 'bstack')
-if runs_on_bstack:
-    load_dotenv(file.abs_path_from_project(f'.env.credentials'))
-
 remote_url = os.getenv('URL_WD_HUB')
 appWaitActivity = os.getenv('APP_WAIT_ACTIVITY')
 
@@ -23,15 +19,25 @@ hub_timeout = os.getenv('HUB_TIMEOUT', 15.0)
 
 def to_driver_options():
     options = UiAutomator2Options()
-    options.set_capability('app', app)
-    options.set_capability('appWaitActivity', appWaitActivity)
 
-    if runs_on_bstack:
+    if (context == 'local_emulator'):
+        options.set_capability('app', app)
+        options.set_capability('appWaitActivity', appWaitActivity)
+
+    elif (context == 'local_real'):
+        options.set_capability('app', app)
+        options.set_capability('appWaitActivity', appWaitActivity)
+
+    elif (context == 'bstack'):
+        load_dotenv(file.abs_path_from_project(f'.env.credentials'))
+
         bstackUserName = os.getenv('BS_USERNAME')
         bstackAccessKey = os.getenv('BS_ACCESSKEY')
         platformVersion = os.getenv('PLATFORM_VERSION')
         deviceName = os.getenv('DEVICE_NAME')
 
+        options.set_capability('app', app)
+        options.set_capability('appWaitActivity', appWaitActivity)
         options.set_capability('deviceName', deviceName)
         options.set_capability('platformVersion', platformVersion)
         options.set_capability(
