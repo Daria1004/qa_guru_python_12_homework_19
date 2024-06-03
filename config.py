cec42f5f96b4af25a68387d8022e71b5dacc7e18
Daria Jakuszewicz <d.jakuszewicz@gmail.com>
2024-06-03 18:29:43 +0300

    Улучшила структуру конфига

--- config.py
+++ config.py
@@ -1,42 +1,38 @@
 import os
 from dotenv import load_dotenv
-
+from appium.options.android import UiAutomator2Options
 from utils import file
 
 context = os.getenv('CONTEXT', 'local_emulator')
 
 load_dotenv(file.abs_path_from_project(f'.env.{context}'))
 
-if context == 'bstack':
+runs_on_bstack = (context == 'bstack')
+if runs_on_bstack:
     load_dotenv(file.abs_path_from_project(f'.env.credentials'))
 
-remote_url = os.getenv('URL_WD_HUB', '')
-deviceName = os.getenv('DEVICE_NANE')
-appWaitActivity = os.getenv('appWaitActivity', 'org.wikipedia.*')
-app = os.getenv('APP_STRING', './app-alpha-universal-release.apk')
-runs_on_bstack = app.startswith('bs://')
-bstackUserName = os.getenv('BS_USERNAME', '')
-bstackAccessKey = os.getenv('BS_ACCESSKEY', '')
+remote_url = os.getenv('URL_WD_HUB')
+appWaitActivity = os.getenv('APP_WAIT_ACTIVITY')
+
+app = os.getenv('APP_STRING')
+if app.startswith('./'):
+    app = file.abs_path_from_project(app)
+
 hub_timeout = os.getenv('HUB_TIMEOUT', 15.0)
-platformVersion = os.getenv('PLATFORM_VERSION', '9.0')
 
 
 def to_driver_options():
-    from appium.options.android import UiAutomator2Options
     options = UiAutomator2Options()
-
-    if deviceName:
-        options.set_capability('deviceName', deviceName)
-
-    if appWaitActivity:
-        options.set_capability('appWaitActivity', appWaitActivity)
-
-    options.set_capability('app', (
-        app if (app.startswith('/') or app.startswith('C:') or runs_on_bstack)
-        else file.abs_path_from_project(app)
-    ))
+    options.set_capability('app', app)
+    options.set_capability('appWaitActivity', appWaitActivity)
 
     if runs_on_bstack:
+        bstackUserName = os.getenv('BS_USERNAME')
+        bstackAccessKey = os.getenv('BS_ACCESSKEY')
+        platformVersion = os.getenv('PLATFORM_VERSION')
+        deviceName = os.getenv('DEVICE_NAME')
+
+        options.set_capability('deviceName', deviceName)
         options.set_capability('platformVersion', platformVersion)
         options.set_capability(
             'bstack:options', {
