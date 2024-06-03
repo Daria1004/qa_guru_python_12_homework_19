7be6eb1ff90591103e905db3386bf41ae1969010
Daria Jakuszewicz <d.jakuszewicz@gmail.com>
2024-06-03 13:06:50 +0300

    add test_getting_started

--- tests/android_app/test_wikipedia.py
+++ tests/android_app/test_wikipedia.py
@@ -1,21 +1,47 @@
 from allure_commons._allure import step
 from appium.webdriver.common.appiumby import AppiumBy
-from selene import browser, have
+from selene import browser, have, be
+
+from utils import file
 
 
-def test_search():
+def test_getting_started():
+    with step('Onboarding 1'):
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/primaryTextView')).should(
+            have.text('The Free Encyclopedia\n…in over 300 languages'))
 
-    with step('Type search'):
-        browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
-        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Appium')
+    with step('Press Continue button'):
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_forward_button')).click()
 
-    with step('Verify content found'):
-        results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
-        results.should(have.size_greater_than(0))
-        results.first.should(have.text('Appium'))
+    with step('Onboarding 2'):
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/primaryTextView')).should(
+            have.text('New ways to explore'))
+
+    with step('Press Continue button'):
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_forward_button')).click()
+
+    with step('Onboarding 3'):
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/primaryTextView')).should(
+            have.text('Reading lists with sync'))
+
+    with step('Press Continue button'):
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_forward_button')).click()
+
+    with step('Onboarding 4'):
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/primaryTextView')).should(
+            have.text('Data & Privacy'))
+
+    with step('Press Get started button'):
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_done_button')).click()
+
+    with step('Main Wikipedia page'):
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/main_toolbar_wordmark')).should(be.visible)
 
 
 def test_search_qa():
+
+    with step('Skip wellcome screen'):
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_skip_button')).click()
 
     with step('Type search'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
